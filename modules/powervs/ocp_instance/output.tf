dc848eff9b0239a7222aeed24e5fe169c41c33b1
git <daniel.casali@gmail.com>
2024-03-05 10:32:09 -0500

    Adding features

--- modules/powervs/ocp_instance/output.tf
+++ modules/powervs/ocp_instance/output.tf
@@ -2,12 +2,18 @@ data "ibm_pi_instances" "ds_instance" {
   pi_cloud_instance_id = var.this_workspace_id
 }
 
-
-output "ocp_instance_mac" {
-  value = {
-    for instance in data.ibm_pi_instances.ds_instance.pvm_instances : instance.pvm_instance_id => {
-      ip_address = instance.networks[0].ip
-      mac_address = instance.networks[0].macaddress
+variable "mac_list" {
+  type = map(any)
+  default = {
+    ocp_instance_mac = {
+      for instance in data.ibm_pi_instances.ds_instance.pvm_instances : instance.pvm_instance_id => {
+        ip_address = instance.networks[0].ip
+        mac_address = instance.networks[0].macaddress
+      }
     }
   }
+}
+
+output "ocp_instance_mac" {
+  value = mac_list
 }
--- modules/powervs/powervs.tf
+++ modules/powervs/powervs.tf
@@ -67,7 +67,7 @@ module "ocp_instance" {
 module "ocp_inst_shut" {
   source     = "./inst_shut"
   depends_on = [module.ocp_instance]
-  instance_mac = module.ocp_instance.ocp_instance_mac
+  instance_mac = module.ocp_instance.ocp_instance_mac.value
   ibmcloud_api_key = var.ibmcloud_api_key
   this_workspace_id = module.workspace.workspace_id
 }
@@ -88,7 +88,7 @@ module "lnx_instance" {
   this_workspace_id = module.workspace.workspace_id
   this_network_id = module.network.this_network_id
   ssh_key_id = module.ssh_key.ssh_key_id
-  ocp_instance_mac = module.ocp_instance.ocp_instance_mac
+  ocp_instance_mac = module.ocp_instance.ocp_instance_mac.value
   this_image_id = each.value.pi_image_id
   provider_region = var.provider_region
   ibmcloud_api_key = var.ibmcloud_api_key
@@ -97,7 +97,7 @@ module "lnx_instance" {
 module "ocp_inst_up" {
   source     = "./inst_up"
   depends_on = [module.lnx_instance]
-  instance_mac = module.ocp_instance.ocp_instance_mac
+  instance_mac = module.ocp_instance.ocp_instance_mac.value
   ibmcloud_api_key = var.ibmcloud_api_key
   this_workspace_id = module.workspace.workspace_id
 }
