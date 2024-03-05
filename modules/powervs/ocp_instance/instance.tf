96500e0071e60a8eec254f3bf2f0850ea66d5cf9
git <daniel.casali@gmail.com>
2024-03-05 11:37:41 -0500

    Adding features

--- modules/powervs/ocp_instance/instance.tf
+++ modules/powervs/ocp_instance/instance.tf
@@ -16,6 +16,10 @@ resource "ibm_pi_instance" "instance" {
 }
 
 
+data "ibm_pi_instances" "ds_instance" {
+  pi_cloud_instance_id = var.this_workspace_id
+}
+
 
 variable "this_pi_instance_name" {
 }
--- modules/powervs/ocp_instance/output.tf
+++ modules/powervs/ocp_instance/output.tf
@@ -1,7 +1,3 @@
-data "ibm_pi_instances" "ds_instance" {
-  pi_cloud_instance_id = var.this_workspace_id
-}
-
 
 output "ocp_instance_mac" {
   value = {
