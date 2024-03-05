2cbc2259ed06ec7729069aa539ba9a8d786f4934
git <daniel.casali@gmail.com>
2024-03-05 11:46:24 -0500

    Adding features

--- modules/powervs/powervs.tf
+++ modules/powervs/powervs.tf
@@ -96,7 +96,7 @@ module "lnx_instance" {
   this_workspace_id = module.workspace.workspace_id
   this_network_id = module.network.this_network_id
   ssh_key_id = module.ssh_key.ssh_key_id
-  ocp_instance_mac = module.ocp_instance
+  ocp_instance_mac = module.get_mac.ocp_instance_mac
   this_image_id = each.value.pi_image_id
   provider_region = var.provider_region
   ibmcloud_api_key = var.ibmcloud_api_key
@@ -105,7 +105,7 @@ module "lnx_instance" {
 module "ocp_inst_up" {
   source     = "./inst_up"
   depends_on = [module.lnx_instance]
-  ocp_instance_mac = module.ocp_instance
+  ocp_instance_mac = module.get_mac.ocp_instance_mac
   ibmcloud_api_key = var.ibmcloud_api_key
   this_workspace_id = module.workspace.workspace_id
 }
