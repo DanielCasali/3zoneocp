4fc5eeb98da181e038f6925bfe0a1f1bb7afaafb
git <daniel.casali@gmail.com>
2024-03-05 11:16:34 -0500

    Adding features

--- modules/powervs/ocp_instance/output.tf
+++ modules/powervs/ocp_instance/output.tf
@@ -1,4 +1,16 @@
+data "ibm_pi_instances" "ds_instance" {
+  pi_cloud_instance_id = var.this_workspace_id
+}
+
 
 output "ocp_instance_mac" {
-  value = mac_list
+  value = {
+    instance_list = {
+      for instance in data.ibm_pi_instances.ds_instance.pvm_instances : instance.pvm_instance_id => {
+        ip_address  = instance.networks[0].ip
+        mac_address = instance.networks[0].macaddress
+      }
+   }
+  }
 }
+
