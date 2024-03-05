785b57aed0a51e2b554c2709ca2f88c4b3dd2f5f
git <daniel.casali@gmail.com>
2024-03-05 11:19:02 -0500

    Adding features

--- modules/powervs/inst_shut/instshut.tf
+++ modules/powervs/inst_shut/instshut.tf
@@ -1,11 +1,11 @@
 resource "ibm_pi_instance_action" "example" {
-  for_each = var.instance_mac
+  for_each = var.ocp_instance_mac
   pi_cloud_instance_id  = var.this_workspace_id
   pi_instance_id        = each.key
   pi_action             = "immediate-shutdown"
 }
 
-variable "instance_mac" {
+variable "ocp_instance_mac" {
 
 }
 variable "ibmcloud_api_key" {}
