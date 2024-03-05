204b3062b023775ca11f4ad56b3986a4337f8c1c
git <daniel.casali@gmail.com>
2024-03-05 11:12:10 -0500

    Adding features

--- modules/powervs/ocp_instance/instance.tf
+++ modules/powervs/ocp_instance/instance.tf
@@ -15,6 +15,23 @@ resource "ibm_pi_instance" "instance" {
   }
 }
 
+data "ibm_pi_instances" "ds_instance" {
+  pi_cloud_instance_id = var.this_workspace_id
+}
+
+
+variable "mac_list" {
+  type = map(any)
+  default = {
+    ocp_instance_mac = {
+      for instance in data.ibm_pi_instances.ds_instance.pvm_instances : instance.pvm_instance_id => {
+        ip_address = instance.networks[0].ip
+        mac_address = instance.networks[0].macaddress
+      }
+    }
+  }
+}
+
 
 variable "this_pi_instance_name" {
 }
--- modules/powervs/ocp_instance/output.tf
+++ modules/powervs/ocp_instance/output.tf
@@ -1,18 +1,3 @@
-data "ibm_pi_instances" "ds_instance" {
-  pi_cloud_instance_id = var.this_workspace_id
-}
-
-variable "mac_list" {
-  type = map(any)
-  default = {
-    ocp_instance_mac = {
-      for instance in data.ibm_pi_instances.ds_instance.pvm_instances : instance.pvm_instance_id => {
-        ip_address = instance.networks[0].ip
-        mac_address = instance.networks[0].macaddress
-      }
-    }
-  }
-}
 
 output "ocp_instance_mac" {
   value = mac_list
