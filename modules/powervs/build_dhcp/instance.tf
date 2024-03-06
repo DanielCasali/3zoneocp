42fcb99d07a72ec5ea4ee0348cdbc63b6e986c8c
git <daniel.casali@gmail.com>
2024-03-06 09:31:35 -0500

    Adding features

--- modules/powervs/build_dhcp/instance.tf
+++ modules/powervs/build_dhcp/instance.tf
@@ -3,24 +3,33 @@ variable "instances" {}
 
 variable "ibmcloud_api_key" {}
 
+data "templatefile" "cloud_init_config" {
+  template = <<-EOF
+#cloud-config
+write_files:
+  - path: /path/to/file.txt
+    content: |
+      $${map_lines}
 
-vars = {
-  map_lines = yamlencode(
-    flatten(
-      [
-        "this is the beginning",
+EOF
+  vars = {
+    map_lines = yamlencode(
+      flatten(
         [
-          for instance_name, instance in var.instances.instance_list : [
-          "# ${instance_name}",
-          join(
-            "\n# This is between key-value pairs of ${instance_name}\n",
-            [for k, v in instance : "${k}=${v}"]
-          ),
-          "something in the end"
+          "this is the beginning",
+          [
+            for instance_name, instance in var.instances.instance_list : [
+            "# ${instance_name}",
+            join(
+              "\n# This is between key-value pairs of ${instance_name}\n",
+              [for k, v in instance : "${k}=${v}"]
+            ),
+            "something in the end"
+          ]
+          ]
         ]
-        ]
-      ]
+      )
     )
-  )
+  }
 }
-}
+
