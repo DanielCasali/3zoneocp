4c42c4a1c44e00619cb91488d954569acf500c34
git <daniel.casali@gmail.com>
2024-03-06 18:56:05 -0500

    Adding features

--- modules/powervs/lnx_image/lnx_image.tf
+++ modules/powervs/lnx_image/lnx_image.tf
@@ -1,4 +1,4 @@
-resource "ibm_pi_image" "centos"{
+resource "ibm_pi_catalog_image" "centos" {
   pi_cloud_instance_id = var.this_workspace_id
   pi_image_id          = "b86c8d00-6419-4652-ba93-4355a5d62ac4"
   pi_image_name        = "CentOSStream8"
