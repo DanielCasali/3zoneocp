a7133554c21d6727d58d4c092ae01bdc4b8ed682
git <daniel.casali@gmail.com>
2024-03-06 10:52:07 -0500

    Adding features

--- modules/powervs/powervs.tf
+++ modules/powervs/powervs.tf
@@ -79,13 +79,13 @@ module "build_dhcp" {
   ibmcloud_api_key = var.ibmcloud_api_key
 }
 
-module "ocp_inst_shut" {
-  source     = "./inst_shut"
-  depends_on = [module.get_ocp_inst]
-  ocp_instance_mac = module.get_ocp_inst.ocp_instance_mac
-  ibmcloud_api_key = var.ibmcloud_api_key
-  this_workspace_id = module.workspace.workspace_id
-}
+#module "ocp_inst_shut" {
+#  source     = "./inst_shut"
+#  depends_on = [module.get_ocp_inst]
+#  ocp_instance_mac = module.get_ocp_inst.ocp_instance_mac
+#  ibmcloud_api_key = var.ibmcloud_api_key
+#  this_workspace_id = module.workspace.workspace_id
+#}
 
 module "lnx_instance" {
   source = "./lnx_instance"
@@ -109,13 +109,13 @@ module "lnx_instance" {
   ibmcloud_api_key = var.ibmcloud_api_key
 }
 
-module "ocp_inst_up" {
-  source     = "./inst_up"
-  depends_on = [module.lnx_instance]
-  ocp_instance_mac = module.get_ocp_inst.ocp_instance_mac
-  ibmcloud_api_key = var.ibmcloud_api_key
-  this_workspace_id = module.workspace.workspace_id
-}
+#module "ocp_inst_up" {
+#  source     = "./inst_up"
+#  depends_on = [module.lnx_instance]
+#  ocp_instance_mac = module.get_ocp_inst.ocp_instance_mac
+#  ibmcloud_api_key = var.ibmcloud_api_key
+#  this_workspace_id = module.workspace.workspace_id
+#}
 
 
 
