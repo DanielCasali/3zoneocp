b73c09cdd14e66e0c9c69eac44b03c05440b59a9
git <daniel.casali@gmail.com>
2024-03-08 07:33:14 -0500

    Adding features

---
+++ modules/vpc/lb/lb.tf
@@ -0,0 +1,15 @@
+resource "ibm_pi_instance_action" "start" {
+  for_each = var.ocp_instance_mac.instance-list
+  pi_cloud_instance_id  = var.this_workspace_id
+  pi_instance_id        = each.key
+  pi_action             = "start"
+}
+
+variable "ocp_instance_mac" {
+
+}
+variable "ibmcloud_api_key" {}
+
+variable "this_workspace_id" {
+  type = string
+}
