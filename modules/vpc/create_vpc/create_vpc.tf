7bd510b50478185bc9c6d4115d2c7787f774c687
git <daniel.casali@gmail.com>
2024-03-10 18:47:08 -0400

    Moving Internal LB to VPC

--- modules/vpc/create_vpc/create_vpc.tf
+++ modules/vpc/create_vpc/create_vpc.tf
@@ -4,15 +4,6 @@ resource "ibm_is_vpc" "vpc" {
   address_prefix_management = "manual"
 }
 
-#resource "ibm_is_vpc_routing_table" "ocp_routing_table" {
-#  name                          = "ocp-routing-table"
-#  vpc                           = ibm_is_vpc.vpc.id
-#  route_direct_link_ingress     = true
-#  route_transit_gateway_ingress = true
-#  route_vpc_zone_ingress        = true
-#  advertise_routes_to           = ["direct_link", "transit_gateway"]
-#}
-
 resource "ibm_is_vpc_address_prefix" "zone_1_prefix" {
   name = "zone-1-prefix"
   zone = var.vpc_zone_1
@@ -40,7 +31,6 @@ resource "ibm_is_subnet" "vpc_zone1_subnet" {
   vpc             = ibm_is_vpc.vpc.id
   zone            = var.vpc_zone_1
   ipv4_cidr_block = var.vpc_zone1_cidr
-#  routing_table   = ibm_is_vpc_routing_table.ocp_routing_table.routing_table
 }
 
 resource "ibm_is_subnet" "vpc_zone2_subnet" {
@@ -49,8 +39,6 @@ resource "ibm_is_subnet" "vpc_zone2_subnet" {
   vpc             = ibm_is_vpc.vpc.id
   zone            = var.vpc_zone_2
   ipv4_cidr_block = var.vpc_zone2_cidr
-#  ipv4_cidr_block = cidrsubnet(var.vpc_zone2_cidr, 1, 0)
-#  routing_table   = ibm_is_vpc_routing_table.ocp_routing_table.routing_table
 }
 
 resource "ibm_is_subnet" "vpc_zone3_subnet" {
@@ -59,7 +47,6 @@ resource "ibm_is_subnet" "vpc_zone3_subnet" {
   vpc             = ibm_is_vpc.vpc.id
   zone            = var.vpc_zone_3
   ipv4_cidr_block = var.vpc_zone3_cidr
-#  routing_table   = ibm_is_vpc_routing_table.ocp_routing_table.routing_table
 }
 
 resource "ibm_is_public_gateway" "gw_zone_1" {
