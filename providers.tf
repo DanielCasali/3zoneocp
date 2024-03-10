34741e184fb63126cae8df2b7b35c047acab5038
git <daniel.casali@gmail.com>
2024-03-10 12:10:44 -0400

    Adding features

--- providers.tf
+++ providers.tf
@@ -1,25 +1,25 @@
 provider ibm {
-  region = var.provider_region
+  region = var.region_entries.region
   ibmcloud_api_key = var.ibmcloud_api_key
 }
 
 provider ibm {
   alias = "powervs1"
-  region = var.provider_region
-  zone = var.zone1
+  region = var.region_entries.region
+  zone = var.region_entries.zone1.pvs_dc_name
   ibmcloud_api_key = var.ibmcloud_api_key
 }
 
 provider ibm {
   alias = "powervs2"
-  region = var.provider_region
-  zone = var.zone2
+  region = var.region_entries.region
+  zone = var.region_entries.zone2.pvs_dc_name
   ibmcloud_api_key = var.ibmcloud_api_key
 }
 
 provider ibm {
   alias = "powervs3"
-  region = var.provider_region
-  zone = var.zone3
+  region = var.region_entries.region
+  zone = var.region_entries.zone3.pvs_dc_name
   ibmcloud_api_key = var.ibmcloud_api_key
 }
