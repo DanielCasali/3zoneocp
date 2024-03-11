8c486709f317a8f6efa72c99b046c9970170e923
git <daniel.casali@gmail.com>
2024-03-11 06:24:09 -0400

    Moving Internal LB to VPC

--- modules/vpc/vpc.tf
+++ modules/vpc/vpc.tf
@@ -36,7 +36,7 @@ module "create_inst1" {
   vpc_id = module.create_vpc.vpc_id
   user_data =  var.vpc_infra_init_config
   zone_name = var.vpc_zone_1
-  image_id = local.vpc_boot_image
+  image_id = local.centos_image_id
   subnet_id = module.create_vpc.subnet1_vpc_id
 }
 
@@ -49,7 +49,7 @@ module "create_inst2" {
   vpc_id = module.create_vpc.vpc_id
   user_data =  var.vpc_infra_init_config
   zone_name = var.vpc_zone_2
-  image_id = local.vpc_boot_image
+  image_id = local.centos_image_id
   subnet_id = module.create_vpc.subnet2_vpc_id
 }
 
@@ -101,18 +101,18 @@ variable "vpc_zone_2" {}
 variable "vpc_zone_3" {}
 variable "vpc_infra_init_config" {}
 
-data "ibm_is_images" "vpc_boot_image" {
+data "ibm_is_images" "vpc" {
   visibility = "public"
   status = "available"
 }
 
 locals {
-  vpc_boot_image = [
-    for image in data.ibm_is_images.vpc_boot_image.images:
+  vpc_image = [
+    for image in data.ibm_is_images.vpc.images:
     image
     if image.os == "centos-stream-9-amd64"
   ][0]
-  centos_image_id = local.vpc_boot_image.id
+  centos_image_id = local.vpc_image.id
 }
 
 
