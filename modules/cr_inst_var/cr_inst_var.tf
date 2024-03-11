701563b07527c6e47e0864a9a59caf0f74594c2e
git <daniel.casali@gmail.com>
2024-03-11 07:45:27 -0400

    Moving Internal LB to VPC

--- modules/cr_inst_var/cr_inst_var.tf
+++ modules/cr_inst_var/cr_inst_var.tf
@@ -187,8 +187,8 @@ packages:
   - squid
   - chrony
 runcmd:
-  - [ /usr/bin/sed, -ie, '/acl SSL_ports port 443/a\'$\n''acl SSL_ports port 6443', /etc/squid/squid.conf ]
-  - [ /usr/bin/sed, -ie, '/acl Safe_ports port 443/a\'$\n''acl Safe_ports port 6443', /etc/squid/squid.conf ]
+  - [ /usr/bin/sed, -ie, '/acl SSL_ports port 443/a acl SSL_ports port 6443', /etc/squid/squid.conf ]
+  - [ /usr/bin/sed, -ie, '/acl Safe_ports port 443/a acl Safe_ports port 6443', /etc/squid/squid.conf ]
   - [ systemctl, enable, squid.service, --now ]
   - [ systemctl, enable, dnsmasq.service, --now ]
   - [ systemctl, restart, squid.service ]
