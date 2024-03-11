02edac73bcf6d1a30e82ffc9ee0b88fc4fd391b5
git <daniel.casali@gmail.com>
2024-03-11 06:45:22 -0400

    Moving Internal LB to VPC

--- modules/vpc/output.tf
+++ modules/vpc/output.tf
@@ -6,27 +6,6 @@ output "vpc_instance2_ip" {
   value = module.create_inst2.ibm_instance_ip
 }
 
-
-output "lb_int_id" {
-  value = module.lb_int.lb_int_id
-}
-
-output "lb_int_pool_apps_id" {
-  value = module.lb_int.lb_int_pool_apps_id
-}
-
-output "lb_int_pool_app_id" {
-  value = module.lb_int.lb_int_pool_app_id
-}
-
-output "lb_int_pool_cfgmgr_id" {
-  value = module.lb_int.lb_int_pool_cfgmgr_id
-}
-
-output "lb_int_pool_api_id" {
-  value = module.lb_int.lb_int_pool_api_id
-}
-
 output "vpc_id" {
   value = module.create_vpc.vpc_id
 }
