abdd6459ae403981f5ca34c32c5b9313821e4907
git <daniel.casali@gmail.com>
2024-03-11 06:41:56 -0400

    Moving Internal LB to VPC

--- modules/cr_inst_var/cr_inst_var.tf
+++ modules/cr_inst_var/cr_inst_var.tf
@@ -183,24 +183,24 @@ write_files:
       bindcmdaddress 0.0.0.0
       logdir /var/log/chrony
   - path: /etc/squid/squid.conf
-    acl localnet src 10.0.0.0/8	# RFC1918 possible internal network
-    acl localnet src 172.16.0.0/12	# RFC1918 possible internal network
-    acl localnet src 192.168.0.0/16	# RFC1918 possible internal network
-    acl localnet src fc00::/7       # RFC 4193 local private network range
-    acl localnet src fe80::/10      # RFC 4291 link-local (directly plugged) machines
+    acl localnet src 10.0.0.0/8
+    acl localnet src 172.16.0.0/12
+    acl localnet src 192.168.0.0/16
+    acl localnet src fc00::/7
+    acl localnet src fe80::/10
     acl SSL_ports port 443
     acl SSL_ports port 6443
-    acl Safe_ports port 80		# http
-    acl Safe_ports port 21		# ftp
-    acl Safe_ports port 443		# https
-    acl Safe_ports port 6443	# api
-    acl Safe_ports port 70		# gopher
-    acl Safe_ports port 210		# wais
-    acl Safe_ports port 1025-65535	# unregistered ports
-    acl Safe_ports port 280		# http-mgmt
-    acl Safe_ports port 488		# gss-http
-    acl Safe_ports port 591		# filemaker
-    acl Safe_ports port 777		# multiling http
+    acl Safe_ports port 80
+    acl Safe_ports port 21
+    acl Safe_ports port 443
+    acl Safe_ports port 6443
+    acl Safe_ports port 70
+    acl Safe_ports port 210
+    acl Safe_ports port 1025-65535
+    acl Safe_ports port 280
+    acl Safe_ports port 488
+    acl Safe_ports port 591
+    acl Safe_ports port 777
     acl CONNECT method CONNECT
     http_access deny !Safe_ports
     http_access deny CONNECT !SSL_ports
@@ -211,10 +211,10 @@ write_files:
     http_access deny all
     http_port 3128
     coredump_dir /var/spool/squid
-    refresh_pattern ^ftp:		1440	20%	10080
-    refresh_pattern ^gopher:	1440	0%	1440
-    refresh_pattern -i (/cgi-bin/|\?) 0	0%	0
-    refresh_pattern .		0	20%	4320
+    refresh_pattern ^ftp: 1440 20% 10080
+    refresh_pattern ^gopher: 1440 0% 1440
+    refresh_pattern -i (/cgi-bin/|\?) 0 0% 0
+    refresh_pattern . 0 20% 4320
 packages:
   - dnsmasq
   - squid
