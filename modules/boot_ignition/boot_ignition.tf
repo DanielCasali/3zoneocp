e3609b3da19375c535d60a723a1ffbb8b440dc37
git <daniel.casali@gmail.com>
2024-03-17 13:23:16 -0400

    Moving Internal LB to VPC

--- modules/boot_ignition/boot_ignition.tf
+++ modules/boot_ignition/boot_ignition.tf
@@ -35,9 +35,23 @@ resource "ibm_iam_access_group_members" "cos_public_access_group_members" {
 }
 
 resource "ibm_iam_authorization_policy" "cos_policy" {
+  source_service_name         = "cloud-object-storage"
+  source_resource_instance_id = ibm_resource_instance.cos_instance.guid
   target_service_name         = "cloud-object-storage"
   target_resource_instance_id = ibm_resource_instance.cos_instance.guid
-  roles                       = ["Object Reader"]
+  roles                       = ["Reader"]
+
+  resource_attributes {
+    name     = "bucket"
+    value    = ibm_cos_bucket.cos_bucket.bucket_name
+    operator = "stringEquals"
+  }
+  resource_attributes {
+    name     = "serviceInstance"
+    value    = ibm_resource_instance.cos_instance.guid
+    operator = "stringEquals"
+  }
+  account_management = false
 }
 
 variable "bootstrap_image" {}
