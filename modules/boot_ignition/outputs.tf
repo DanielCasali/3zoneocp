aebfdbd3e1ebaac28948e2a12563c807e9bf9d34
git <daniel.casali@gmail.com>
2024-03-17 11:21:17 -0400

    Moving Internal LB to VPC

--- modules/boot_ignition/outputs.tf
+++ modules/boot_ignition/outputs.tf
@@ -6,14 +6,14 @@ output "bootstrap_init_file" {
 data "template_file" "bootstrap_init_config" {
   template = <<-EOF
 {
-  "ignition":{
+  "ignition": {
     "version": "3.2.0",
+    "proxy": {
+      "httpProxy": "http://proxy.${var.ocp_cluster_name}.${var.ocp_cluster_domain}:8080",
+      "httpsProxy": "http://proxy.${var.ocp_cluster_name}.${var.ocp_cluster_domain}:8080",
+      "noProxy": [ ".apps.${var.ocp_cluster_name}.${var.ocp_cluster_domain}", "api.${var.ocp_cluster_name}.${var.ocp_cluster_domain}", "api-int.${var.ocp_cluster_name}.${var.ocp_cluster_domain}", "${var.zone1_vpc_zone_cidr}", "${var.zone2_vpc_zone_cidr}", "${var.zone3_vpc_zone_cidr}", "${var.zone1_pvs_dc_cidr}", "${var.zone2_pvs_dc_cidr}", "${var.zone3_pvs_dc_cidr}" ]
+    },
     "config": {
-      "proxy": {
-        "httpProxy": "http://proxy.${var.ocp_cluster_name}.${var.ocp_cluster_domain}:8080",
-        "httpsProxy": "http://proxy.${var.ocp_cluster_name}.${var.ocp_cluster_domain}:8080",
-        "noProxy": [ ".apps.${var.ocp_cluster_name}.${var.ocp_cluster_domain}", "api.${var.ocp_cluster_name}.${var.ocp_cluster_domain}", "api-int.${var.ocp_cluster_name}.${var.ocp_cluster_domain}", "${var.zone1_vpc_zone_cidr}", "${var.zone2_vpc_zone_cidr}", "${var.zone3_vpc_zone_cidr}", "${var.zone1_pvs_dc_cidr}", "${var.zone2_pvs_dc_cidr}", "${var.zone3_pvs_dc_cidr}" ]
-      },
       "replace": {
         "source": "https://s3.${ibm_cos_bucket.cos_bucket.region_location}.cloud-object-storage.appdomain.cloud/${ibm_cos_bucket.cos_bucket.bucket_name}/${ibm_cos_bucket_object.bootstrap.key}"
       }
