20f9a51d2d637cc3204041b5161f6ab7b4af2779
git <daniel.casali@gmail.com>
2024-03-18 18:08:11 -0400

    Moving Internal LB to VPC

--- modules/powervs/get_ocp_inst/output.tf
+++ modules/powervs/get_ocp_inst/output.tf
@@ -8,8 +8,8 @@ locals {
   prod = {
     instance_list = {
       for instance in data.ibm_pi_instances.ds_instance.pvm_instances : instance.pvm_instance_id => {
-        ip_address  = instance.networks[0].ip
-        mac_address = instance.networks[0].macaddress
+        ip_address    = instance.networks[0].ip
+        mac_address   = instance.networks[0].macaddress
       }
     }
   }
