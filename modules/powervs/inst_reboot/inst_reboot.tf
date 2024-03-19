a06c299709b80797baaac5fb3a0f15576221fc44
git <daniel.casali@gmail.com>
2024-03-19 06:33:07 -0400

    Moving Internal LB to VPC

--- modules/powervs/inst_reboot/inst_reboot.tf
+++ modules/powervs/inst_reboot/inst_reboot.tf
@@ -4,19 +4,18 @@ data "ibm_pi_instances" "ds_instance" {
 }
 
 locals {
-  instance_ids_to_reboot = toset([
+  instance_ids_to_reboot = [
     for instance in data.ibm_pi_instances.ds_instance.pvm_instances :
     instance.pvm_instance_id if contains(keys(var.ocp_instance_mac.instance_list), instance.id)
-  ])
+  ]
 }
 
 resource "ibm_pi_instance_action" "reboot" {
-  for_each = local.instance_ids_to_reboot
+  count = length(local.instance_ids_to_reboot)
   pi_cloud_instance_id = var.this_workspace_id
-  pi_instance_id       = each.value
+  pi_instance_id       = local.instance_ids_to_reboot[count.index]
   pi_action            = "hard-reboot"
 }
-
 
 variable "ocp_instance_mac" {
 
